ef0688f84087694d6302057cd1d2beef10ce0c9a
saraXX <sara99.edu@gmail.com>
2022-05-08 11:04:06 +0300

    first commit - add fragments

---
+++ app/src/main/java/android/guide/fragments_part1/FragmentA.java
@@ -0,0 +1,10 @@
+package android.guide.fragments_part1;
+
+import androidx.fragment.app.Fragment;
+
+// TODO 3 : CREATE FRAGMENT CLASS
+public class FragmentA extends Fragment {
+    public FragmentA() {
+        super(R.layout.fragment_a);
+    }
+}
