a846ff9bf9f26afc4f60029f264d9583a00dc3ca
saraXX <sara99.edu@gmail.com>
2022-05-08 11:03:51 +0300

    first commit - add fragments

--- app/src/main/java/android/guide/fragments_part1/MainActivity.java
+++ app/src/main/java/android/guide/fragments_part1/MainActivity.java
@@ -3,12 +3,13 @@ package android.guide.fragments_part1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//todo 0 this app show how to make a simple one fragment without coding 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
